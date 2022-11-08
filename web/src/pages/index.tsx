d6152d4034dfcf5d839e4d8558369a48332041eb
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-11-08 11:16:08 -0700

    View count

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,7 +6,7 @@ function DisplayQuote({ quote }: { quote: Quote }) {
   return (
     <div className="flex flex-col items-center">
       <p>{quote.quote}</p>
-      <span className="pt-4 font-light italic">- {quote.source}</span>
+      <span className="pt-8 font-light italic">- {quote.source}</span>
     </div>
   );
 }
@@ -14,10 +14,8 @@ function DisplayQuote({ quote }: { quote: Quote }) {
 function DisplayWord({ word }: { word: Word }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex flex-row items-center pb-4">
-        <span className="font-bold">{word.word}</span>
-        <span className="pl-2 text-sm italic sm:text-lg">({word.pos})</span>
-      </div>
+      <span className="font-bold">{word.word}</span>
+      <span className="my-2 text-sm italic sm:text-lg">({word.pos})</span>
       <span>{word.definition}</span>
     </div>
   );
@@ -29,9 +27,15 @@ export default function Home() {
   // useInterval(() => query.refetch(), 3000);
 
   return (
-    <div className="text-lg sm:text-xl lg:text-2xl">
+    <div
+      className="w-screen max-w-2xl cursor-pointer p-4 text-lg sm:text-xl lg:text-2xl"
+      onClick={() => query.refetch()}
+    >
       {!!query.data?.quote && <DisplayQuote quote={query.data.quote} />}
       {!!query.data?.word && <DisplayWord word={query.data.word} />}
+      <div className="my-6 flex w-full flex-col items-center opacity-60">
+        <span className="text-xs">Viewed {query.data?.queried} times</span>
+      </div>
     </div>
   );
 }
