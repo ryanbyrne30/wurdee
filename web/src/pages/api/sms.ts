53a43f07f925db673695bc0e1748e2c76b4d5628
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-11-07 17:49:19 -0700

    /api/sms POST

--- web/src/pages/api/sms.ts
+++ web/src/pages/api/sms.ts
@@ -4,8 +4,11 @@ import twilio from 'twilio';
 import { env } from "../../env/server.mjs";
 
 const client = twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+const MessagingResponse = twilio.twiml.MessagingResponse;
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("Received request:", req.method);
+  console.log(req.body)
   if (req.method === 'GET') {
     client.messages.create({
       body: "Testing from Twilio",
@@ -13,6 +16,14 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       to: env.TWILIO_PHONE_TO
     })
     res.status(200).json({message: "Hello"})
+  }
+
+  if (req.method === 'POST') {
+    const response = new MessagingResponse();
+    response.message("Testing a response");
+    console.log(response.toString())
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(response.toString());
   }
 }
 
